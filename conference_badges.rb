545e07664690d18ac6dcc2b324071f11f95d5bd4
Tirzah P. Howard <tp.howard47@gmail.com>
2020-09-17 07:41:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+def badge_maker(speaker)
+ return "Hello, my name is #{speaker}."
+end
+
+speakers_lineup = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator(speakers_lineup)
+ speakers_lineup.collect {|speaker| badge_maker(speaker)}
+end
